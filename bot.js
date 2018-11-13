504219177ce6e0658a6db0f3bcb63b8d64004023
EdiTeD's Projects <42066039+editedprojects@users.noreply.github.com>
2018-11-13 09:03:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,25 +4,35 @@ const fs = require("fs");
 const sql = require("better-sqlite3");
 const client = new Discord.Client();
 const Codes = new Discord.Client();
- const prefix = "-";
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 
+function checks(){
+        setInterval(() => {
+            client.channels.get('511797914872315905').send('#credits');
+        }, 640000);
+}
+
+client.on("ready", () => {
+let channel =     client.channels.get("511797750644342809")
+setInterval(function() {
+channel.send('edited 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ edited 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخة');
+}, 25)
+ checks();
+})
+ 
+
+
 client.on('message', message => {
-    if (message.content === 'spam') {
-          let count = 0;
-          let ecount = 0;
-          for(let x = 0; x < 100; x++) {
-            message.channel.send(`سمك فوق الجمع `)
-              .then(m => {
-                count++;
-              })
-              
-            }
-          }
-    });
+    let prefix = '11';
+   if(message.content.startsWith(prefix + 'say')) {
+       let args = message.content.split(' ').slice(1).join(' ');
+       message.channel.send(args)
+   } 
+});
+
     
     
     client.login(process.env.BOT_TOKEN); 
